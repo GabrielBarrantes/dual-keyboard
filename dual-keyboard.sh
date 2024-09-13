6fb3521e4e9bae690590f8d46e0a03d7d18bb9c7
Gabriel Barrantes <gabriel.barrantes.dev@outlook.com>
2024-09-13 14:31:39 -0600

    Initial commit

---
+++ dual-keyboard.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# $evtest # to find where is the keyboard
+source env/bin/activate
+sudo python3 main.py 6
---
+++ main.py
@@ -0,0 +1,61 @@
+import os, sys
+import keyboard
+from evdev import InputDevice, categorize, ecodes, UInput
+
+event_number = sys.argv[1]
+device_path = f'/dev/input/event{event_number}'
+dev = InputDevice(device_path)
+dev.grab()
+
+print(f'Listening for key presses on {device_path}...')
+
+for event in dev.read_loop():
+    if event.type == ecodes.EV_KEY:
+        key = categorize(event)
+        print("key code:", key.keycode)
+        if key.keystate == key.key_down:
+            if key.keycode == 'KEY_LEFTSHIFT':
+                keyboard.press_and_release('ctrl+c')
+            elif key.keycode == 'KEY_LEFTCTRL':
+                keyboard.press_and_release('ctrl+v')
+            elif key.keycode == 'KEY_CAPSLOCK':
+                keyboard.press_and_release('ctrl+s')
+            elif key.keycode == 'KEY_LEFTMETA':
+                keyboard.press_and_release('up')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_S':
+                keyboard.press_and_release('up')
+                keyboard.press_and_release('up')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_SPACE':
+                keyboard.press_and_release('up')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_Z':
+                keyboard.press_and_release('ctrl+plus')
+            elif key.keycode == 'KEY_A':
+                keyboard.press_and_release('ctrl+minus')
+            elif key.keycode == 'KEY_F1':
+                keyboard.press_and_release('ctrl+w')
+            elif key.keycode == 'KEY_GRAVE':
+                keyboard.write('git status')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_1':
+                keyboard.write('git checkout ')
+            elif key.keycode == 'KEY_2':
+                keyboard.write('git checkout -b ')
+            elif key.keycode == 'KEY_3':
+                keyboard.write('git add .')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_4':
+                keyboard.write('git commit --amend')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_5':
+                keyboard.write('git commit')
+                keyboard.press_and_release('enter')
+            elif key.keycode == 'KEY_ESC':
+                keyboard.press_and_release('escape')
+            elif key.keycode == 'KEY_TAB':
+                keyboard.press_and_release('ctrl+z')
+
+
+
